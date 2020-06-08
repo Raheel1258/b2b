6bad6436385eb52bfa6192fafef308fc4592d158
Rahil Ahmad <rahilahmad1258@gmail.com>
2020-06-08 05:20:15 -0700

    Updates

---
+++ precache-manifest.38004252b233de47e52c2ef420487b48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08f2c0cd4a30b5e1c1e92e12b91eff1",
+    "url": "/b2b/index.html"
+  },
+  {
+    "revision": "4399a5341ea69b2a4984",
+    "url": "/b2b/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8a323bd3b4c2613281df",
+    "url": "/b2b/static/css/main.8d5d5297.chunk.css"
+  },
+  {
+    "revision": "4399a5341ea69b2a4984",
+    "url": "/b2b/static/js/2.f297c789.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/b2b/static/js/2.f297c789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a323bd3b4c2613281df",
+    "url": "/b2b/static/js/main.da7f4dec.chunk.js"
+  },
+  {
+    "revision": "9dbed511f8afc62c4f0c",
+    "url": "/b2b/static/js/runtime-main.6165924b.js"
+  },
+  {
+    "revision": "1f24593f760b465b835febf26bd33ff7",
+    "url": "/b2b/static/media/location.1f24593f.png"
+  }
+]);
